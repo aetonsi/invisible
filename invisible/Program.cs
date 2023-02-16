7a2d9ddd4f89a36eb29c6596bab4f4dc577f1a64
aetonsi <18366087+aetonsi@users.noreply.github.com>
2023-02-16 22:42:25 +0100

    complete rework with CliWrap, added --stdout and --stderr arguments

--- invisible/Program.cs
+++ invisible/Program.cs
@@ -1,36 +1,17 @@
-﻿// TODO #CLIWRAP implement everything via https://github.com/Tyrrrz/CliWrap !!!
+﻿using CliWrap;
+using System.Diagnostics;
 
-﻿using System.Diagnostics;
-
-string __FILE__ = System.Environment.ProcessPath ?? Process.GetCurrentProcess().ProcessName;
-Func<string, string[], string, bool, Process> my_process = (program, args, working_dir, redirectStreams) =>
-{
-    ProcessStartInfo psi = new ProcessStartInfo
-    {
-        FileName = program,
-        WorkingDirectory = working_dir,
-        UseShellExecute = false,
-        CreateNoWindow = true,
-        RedirectStandardInput = false, // TODO #stdin8542102154 allow standard input somehow?
-        RedirectStandardOutput = redirectStreams,
-        RedirectStandardError = redirectStreams,
-        WindowStyle = ProcessWindowStyle.Hidden,
-    };
-    foreach (string arg in args) psi.ArgumentList.Add(arg);
-    Process p = new Process();
-    p.StartInfo = psi;
-    return p;
-};
-
-
-
-int exitCode = 0;
-string[] invocationArguments = Environment.GetCommandLineArgs().Skip(1).ToArray();
+////// default values
 string working_dir = Directory.GetCurrentDirectory();
-bool no_output = true;
+bool show_output = false;
 bool always_success = false;
-bool dont_wait = false;
+bool wait_for_exit = true;
+string? stdout_file = null;
+string? stderr_file = null;
+//////
 
+////// parse command line args
+string[] invocationArguments = Environment.GetCommandLineArgs().Skip(1).ToArray();
 while (invocationArguments.Length > 0 && invocationArguments[0].StartsWith("--"))
 {
     int to_shift = 1;
@@ -39,76 +20,93 @@ while (invocationArguments.Length > 0 && invocationArguments[0].StartsWith("--")
         case "--": goto stop_args;
         case "--help": goto print_help;
         case "--working-directory": working_dir = invocationArguments[1]; to_shift = 2; break;
-        case "--show-output": no_output = false; break;
+        case "--show-output": show_output = true; break;
+        case "--stdout": stdout_file = invocationArguments[1]; to_shift = 2; break;
+        case "--stderr": stderr_file = invocationArguments[1]; to_shift = 2; break;
         case "--always-success": always_success = true; break;
-        case "--dont-wait": dont_wait = true; break;
+        case "--dont-wait": wait_for_exit = false; break;
         default: Console.Error.WriteLine($"Unrecognized option: {invocationArguments[0]}"); Environment.Exit(1); break;
     }
     invocationArguments = invocationArguments.Skip(to_shift).ToArray();
 }
 stop_args:;
-
+// check args errors
 if (invocationArguments.Length == 0)
 {
-    Console.Error.WriteLine("No executable specified. Showing --help.\n");
-    if (!always_success) exitCode = 1;
-    goto print_help;
+    Console.Error.WriteLine("No executable specified.\n");
+    Environment.Exit(1);
 }
-
 string program = invocationArguments[0];
 string[] program_args = invocationArguments.Skip(1).ToArray();
+//////
 
-Process p = my_process(program, program_args, working_dir, !no_output);
-p.Start();
+////// invocation
+Command cmd = Cli.Wrap(program) // TODO #stdin8542102154 allow standard input
+    .WithArguments(program_args, true)
+    .WithWorkingDirectory(working_dir)
+    .WithValidation(CommandResultValidation.None);
+CommandTask<CommandResult> task;
+CommandResult? result = null;
 
-if (!dont_wait)
+if (wait_for_exit)
 {
-    if (!no_output)
+    List<PipeTarget> stdout_targets = new List<PipeTarget>();
+    List<PipeTarget> stderr_targets = new List<PipeTarget>();
+    if (show_output)
     {
-        p.ErrorDataReceived += (sender, args) => Console.Error.Write(args.Data != null ? args.Data + Environment.NewLine : "");
-        p.OutputDataReceived += (sender, args) => Console.Write(args.Data != null ? args.Data + Environment.NewLine : "");
-        p.BeginErrorReadLine();
-        p.BeginOutputReadLine();
+        stdout_targets.Add(PipeTarget.ToStream(Console.OpenStandardOutput()));
+        stderr_targets.Add(PipeTarget.ToStream(Console.OpenStandardError()));
     }
-    p.WaitForExit();
+    if (stdout_file != null) stdout_targets.Add(PipeTarget.ToFile(stdout_file));
+    if (stderr_file != null) stderr_targets.Add(PipeTarget.ToFile(stderr_file));
+
+    cmd = cmd.WithStandardOutputPipe(PipeTarget.Merge(stdout_targets.ToArray()));
+    cmd = cmd.WithStandardErrorPipe(PipeTarget.Merge(stderr_targets.ToArray()));
+
+    task = cmd.ExecuteAsync();
+    result = await task;
+}
+else
+{
+    task = cmd.ExecuteAsync();
 }
 
-exitCode = always_success || dont_wait ? 0 : p.ExitCode;
-Environment.Exit(exitCode);
+int exit_code = always_success || !wait_for_exit ? 0 : result.ExitCode;
+Environment.Exit(exit_code);
 
 
 
+///////////////////////////////////////
 
 
 print_help:;
-string f = Path.GetFileName(__FILE__);
+string f = Path.GetFileName(System.Environment.ProcessPath ?? Process.GetCurrentProcess().ProcessName);
 // TODO #helptxt8548745 move help to txt template file
-Console.WriteLine(f + "\n\nInvokes a program silently." +
+Console.WriteLine(f +
+    "\n\nInvokes a program silently." +
     "\nFeatures:" +
-    "\n- can be passed any number of arguments to the program" +
     "\n- can locate the executable via relative path, absolute path, or the system's PATH" +
-    "\n- runs the program in the current directory (or the given directory, via parameter)" +
+    "\n- can run the program with a different starting directory (via parameter)" +
     "\n- waits for the program to be over (or doesn't, via parameter)" +
     "\n- exits with the invocation's exit code (or doesn't, via parameter)" +
-    "\n- if requested via parameter, outputs the invocation's output" +
+    "\n- if requested (via parameters), outputs the invocation's output to the console or files (binary safe)" +
     "\n\nUsage:" +
     "\n    " + f + " [parameters] executable [executable arguments]" +
     "\n\nParameters: " +
     "\n    [--]                                   stops parsing arguments and start parsing executable" +
-    "\n    [--working-directory directory]        runs the program in the given directory" +
-    "\n    [--show-output]                        shows the invocation's shell output" +
+    "\n    [--working-directory <directory>]      runs the program with the given starting directory" +
+    "\n    [--show-output]                        prints the invocation's output to console (both stdout and stderr)" +
+    "\n    [--stdout <filepath>]                  appends the stdout stream to the given file" +
+    "\n    [--stderr <filepath>]                  appends the stderr stream to the given file" +
     "\n    [--always-success]                     always quits with exit code 0, ignoring the invocation's exit code" +
     "\n    [--dont-wait]                          doesn't wait for the invoked program to be over, immediately quits with exit code 0" +
-    "\n\nUsage examples:" +
+    "\n\nUsage examples (NB: escape special characters according to your shell's rules):" +
     "\n    " + f + " myprogram --with -its /arguments list" +
-    "\n    " + f + " --always-success --dont-wait -- myprogram --with -its /arguments list" +
+    "\n    " + f + " --always-success --dont-wait -- myscript.cmd /with /args" +
     "\n    " + f + " ..\\some\\program\\relative\\to\\the\\current\\directory.exe" +
     "\n    " + f + " cmd /c shutdown /p" +
+    "\n    " + f + " --stdout out.txt --stderr err.txt --show-output cmd /c \"echo my output & echo my err >&2\"" +
     "\n    " + f + " logoff" +
-    "\n    " + f + " c:\\absolute\\path\\to\\php.exe -r \"file_put_contents('my_output_file.txt', \\\"double quoted string with spaces\\\");\"" +
-    "\n\nPLEASE NOTE:" +
-    "\nYou have to C-style escape double quotes if you want to pass them literally to the executable, like the php example above. Please also take into consideration the current shell's escape rules for quotes." +
-    "\n\nPLEASE NOTE:" +
-    "\nDue to output buffering differences between stdout and stderr, if the invocation sends data to both streams, --show-output will probably print things NOT in the correct order. The only way to be sure to have order, is to have a single stream, that is, merging the streams with redirection with 2>&1 (remember to escape this correctly)." +
+    "\n    " + f + " c:\\absolute\\path\\to\\program.exe" +
     "");
-Environment.Exit(exitCode);
+Environment.Exit(0);
